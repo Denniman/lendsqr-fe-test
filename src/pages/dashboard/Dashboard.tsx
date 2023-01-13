6a78d38e515dd6abd76ca9193218dbe1c2a131da
denniman <dennimanadams@gmail.com>
2023-01-13 23:23:15 +0100

    bug: fix filter positioning

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,27 +1,47 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { RiFilter3Line } from "react-icons/ri";
 import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
 import { BiDotsHorizontalRounded } from "react-icons/bi";
 
 import { useFetch } from "@app/hooks";
-import { DataTable, MetricsCard, Skeleton } from "@app/components/molecules";
+import { UsersContext } from "@app/context";
 import { Select } from "@app/components/atoms";
+import {
+  Filter,
+  Skeleton,
+  DataTable,
+  MetricsCard,
+} from "@app/components/molecules";
 
 const BASE_URL =
   "https://6270020422c706a0ae70b72c.mockapi.io/lendsqr/api/v1/users";
 
 export const Dashboard = () => {
+  const { getUserId } = useContext(UsersContext);
   const [usersPerPage] = useState(10);
   const { data, isLoading, error } = useFetch(BASE_URL);
   const [currentPage, setCurrentPage] = useState(1);
+  const [showFilterDropdown, setShowFilterDropdown] = useState(false);
 
   const lastUserIndex = currentPage * usersPerPage;
   const firstUsersIndex = lastUserIndex - usersPerPage;
   const currentUsers = data?.slice(firstUsersIndex, lastUserIndex);
   const pageCount = firstUsersIndex + Number(currentUsers?.length);
 
+  const disablePrevButton = firstUsersIndex === 0;
+  const disableNextButton = pageCount === data?.length;
+
+  const errorMessage = !data && error;
+  const navigate = useNavigate();
+
+  const closeFilter = () => {
+    if (!showFilterDropdown) return;
+    setShowFilterDropdown(false);
+  };
+
   return (
-    <div className="dashboard">
+    <div className="dashboard" onClick={closeFilter}>
       <p className="page-title">Dashboard</p>
       <div className="metrics__wrapper">
         <MetricsCard type="users" title="Users" stat="2,453" />
@@ -35,42 +55,68 @@ export const Dashboard = () => {
       </div>
       <div className="table__wrapper">
         <div className="table__header">
-          <div className="table__header--item">
+          <div
+            className="table__header--item"
+            onClick={() => setShowFilterDropdown(!showFilterDropdown)}
+          >
             <p className="table__title">Organization</p>
             <RiFilter3Line className="filter--icon" />
+            {showFilterDropdown && <Filter className="filter__dropdown" />}
           </div>
-          <div className="table__header--item">
+          <div
+            className="table__header--item"
+            onClick={() => setShowFilterDropdown(!showFilterDropdown)}
+          >
             <p className="table__title">Username</p>
             <RiFilter3Line className="filter--icon" />
           </div>
 
-          <div className="table__header--item ">
+          <div
+            className="table__header--item"
+            onClick={() => setShowFilterDropdown(!showFilterDropdown)}
+          >
             <p className="table__title">Email</p>
             <RiFilter3Line className="filter--icon" />
           </div>
-          <div className="table__header--item show">
+          <div
+            className="table__header--item show"
+            onClick={() => setShowFilterDropdown(!showFilterDropdown)}
+          >
             <p className="table__title">Phone number</p>
             <RiFilter3Line className="filter--icon" />
           </div>
-          <div className="table__header--item show">
+          <div
+            className="table__header--item show"
+            onClick={() => setShowFilterDropdown(!showFilterDropdown)}
+          >
             <p className="table__title">Date joined</p>
             <RiFilter3Line className="filter--icon" />
           </div>
-          <div className="table__header--item show">
+          <div
+            className="table__header--item show"
+            onClick={() => setShowFilterDropdown(!showFilterDropdown)}
+          >
             <p className="table__title">Status</p>
             <RiFilter3Line className="filter--icon" />
           </div>
         </div>
 
-        {error && (
+        {errorMessage && (
           <div className="error">
-            <p>{`Error: ${error}`}</p>
+            <p className="error--text">{`Error: ${error}`}</p>
           </div>
         )}
 
         {isLoading && <Skeleton length={6} />}
         {currentUsers?.map((users) => (
-          <DataTable key={users.id} {...users} onViewDetails={() => {}} />
+          <DataTable
+            key={users.id}
+            {...users}
+            onViewDetails={() => {
+              getUserId(users.id);
+              navigate("/user-details", { replace: true });
+            }}
+          />
         ))}
       </div>
       <div className="table__footer">
@@ -84,7 +130,7 @@ export const Dashboard = () => {
         <div className="table__footer--pagination">
           <button
             className="pagination--btn"
-            disabled={firstUsersIndex === 0}
+            disabled={disablePrevButton}
             onClick={() => setCurrentPage(currentPage - 1)}
           >
             <MdKeyboardArrowLeft />
@@ -105,7 +151,7 @@ export const Dashboard = () => {
           <p>16</p>
           <button
             className="pagination--btn"
-            disabled={pageCount === data?.length}
+            disabled={disableNextButton}
             onClick={() => setCurrentPage(currentPage + 1)}
           >
             <MdKeyboardArrowRight />
