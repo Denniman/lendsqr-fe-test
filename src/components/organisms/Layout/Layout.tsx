6cd6a273abd314fd937850c411e9ff2372bf9c0e
denniman <dennimanadams@gmail.com>
2023-01-13 23:22:09 +0100

    bug: fix issue with side bar not closing after nagivation

--- src/components/organisms/Layout/Layout.tsx
+++ src/components/organisms/Layout/Layout.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 import { Header } from "@app/components/molecules";
 import { Sidebar } from "../Sidebar";
@@ -7,14 +8,17 @@ import { LayoutProps } from "./Layout.interface";
 
 export const Layout: React.FC<LayoutProps> = ({ children }) => {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    setIsDrawerOpen(false);
+  }, [pathname]);
   return (
     <div className="layout__wrapper">
-      <Sidebar
-        closeSideBar={() => setIsDrawerOpen(false)}
-        className={`${isDrawerOpen ? "show" : "hide"} sidebar`}
-      />
+      <Sidebar className={`${isDrawerOpen ? "show" : "hide"} sidebar`} />
       <Header
         className="header"
+        isDrawerOpen={isDrawerOpen}
         closeSideBar={() => setIsDrawerOpen(!isDrawerOpen)}
       />
       <main className="layout__main">{children}</main>
